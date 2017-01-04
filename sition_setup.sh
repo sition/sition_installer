ed51a6a6ab1472be4cf95bd8ebe2fe6fda418322
Michel Doens <michel.doens@sition.nl>
2017-01-04 12:41:23 +0100

    aanpassing setup

--- sition_setup.sh
+++ sition_setup.sh
@@ -4,19 +4,30 @@ cd "$(dirname "$0")"
 echo  "starting magento 2 dutch configuration"
 sleep 3
 cd ../
+#find . -type f -exec chmod 644 {} \;      // 644 permission for files
+#find . -type d -exec chmod 755 {} \;      // 755 permission for directory
+find ./var -type d -exec chmod 777 {} \;  // 777 permission for var folder
+
+#find ./pub/media -type d -exec chmod 777 {} \;
+find ./pub/static -type d -exec chmod 777 {} \;
+#chmod 777 ./app/etc
+#chmod 644 ./app/etc/*.xml
+
 find . -name ".htaccess" -type f -exec sed -i 's/FollowSymLinks/SymLinksIfOwnerMatch/g' {} ";"
 find . -name ".htaccess" -type f -exec sed -i 's/All -Indexes/ -Indexes/g' {} ";"
 composer require honl/magento2-nl-nl
 composer require semaio/magento2-configimportexport
 composer update
 php bin/magento setup:upgrade
+touch pub/static/deployed_version.txt
 php bin/magento setup:di:compile
 php bin/magento module:enable Ho_nl_NL
 php bin/magento module:enable Semaio_ConfigImportExport
 php bin/magento sampledata:deploy
 php bin/magento setup:static-content:deploy
 php bin/magento setup:static-content:deploy de_DE
-php bin/magento setup:static-content:deploy nl_NL
+elsels
+
 crontab -l > sition_installer/oldcron.txt
 sleep 5
 crontab sition_installer/newcron.txt
@@ -28,3 +39,4 @@ cp  sition_installer/20167103_sition.yaml config/store/base
 php bin/magento config:data:import config/store base
 n98-magerun2 cache:flush
 n98-magerun2 index:reindex
+cd p
