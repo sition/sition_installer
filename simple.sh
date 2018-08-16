4612ce48e4f981929b0ba2c51bbf90abf22617cb
Michel <micheldoens@hotmail.com>
2018-08-16 23:22:56 +0200

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -16,13 +16,12 @@
 find . -name ".htaccess" -type f -exec sed -i 's/FollowSymLinks/SymLinksIfOwnerMatch/g' {} ";"
 find . -name ".htaccess" -type f -exec sed -i 's/All -Indexes/ -Indexes/g' {} ";"
 composer require honl/magento2-nl-nl
-composer require semaio/magento2-configimportexport
 composer update
 php bin/magento setup:upgrade
 touch pub/static/deployed_version.txt
 php bin/magento setup:di:compile
 php bin/magento module:enable Ho_nl_NL
-php bin/magento module:enable Semaio_ConfigImportExport
 php bin/magento sampledata:deploy
-php bin/magento setup:static-content:deploy
-php bin/magento setup:static-content:deploy de_DE nl_NL en_US
+php bin/magento setup:static-content:deploy -f
+php bin/magento setup:static-content:deploy de_DE nl_NL en_US -f
+php bin/magento setup:di:compile
