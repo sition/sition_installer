f2ff73acf32fbfba0dae7c6595aa25085e7e77e3
Michel <micheldoens@hotmail.com>
2018-08-16 23:33:19 +0200

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -1,18 +1,4 @@
 #! /bin/bash
-
-#PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-#cd "$(dirname "$0")"
-#echo  "starting magento 2 dutch configuration"
-#sleep 3
-#cd ../
-#find . -type f -exec chmod 644 {} \;
-#find . -type d -exec chmod 755 {} \;
-#find ./var -type d -exec chmod 777 {} \;
-#find ./pub/media -type d -exec chmod 777 {} \;
-#find ./pub/static -type d -exec chmod 777 {} \;
-#chmod 777 ./app/etcnano
-#chmod 644 ./app/etc/*.xml
-
 find . -name ".htaccess" -type f -exec sed -i 's/FollowSymLinks/SymLinksIfOwnerMatch/g' {} ";"
 find . -name ".htaccess" -type f -exec sed -i 's/All -Indexes/ -Indexes/g' {} ";"
 composer require honl/magento2-nl-nl
@@ -25,4 +11,3 @@ php bin/magento sampledata:deploy
 php bin/magento setup:static-content:deploy de_DE nl_NL en_US -f
 php bin/magento setup:di:compile
 n98-magerun2 index:reindex
-
