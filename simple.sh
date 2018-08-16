1ba18f9d972d58609eed9f50dabfdea57a75de14
Michel <micheldoens@hotmail.com>
2018-08-16 23:32:04 +0200

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -19,9 +19,10 @@ composer require honl/magento2-nl-nl
 composer update
 php bin/magento setup:upgrade
 touch pub/static/deployed_version.txt
-php bin/magento setup:di:compile
 php bin/magento module:enable Ho_nl_NL
 php bin/magento sampledata:deploy
-php bin/magento setup:static-content:deploy -f
+#php bin/magento setup:static-content:deploy -f
 php bin/magento setup:static-content:deploy de_DE nl_NL en_US -f
 php bin/magento setup:di:compile
+n98-magerun2 index:reindex
+
